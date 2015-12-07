d2719ee955060d5c3840fc323cad8e3c62b8966b
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-07 22:37:24 +0200

    Add customer has_many customer_feedbacks

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@ class Customer < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :customer_feedbacks
 end
