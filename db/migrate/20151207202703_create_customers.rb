3ebbc2904a42ecb6afb115fb66bd391a6c4e84df
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-07 22:33:31 +0200

    Add Customer model with devise

---
+++ db/migrate/20151207202703_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :first_name, null: false
+      t.string :last_name
+      t.string :email, null: false
+      t.string :phone_number
+      t.string :city
+      t.string :country
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :customer do
+    first_name "MyString"
+last_name "MyString"
+email "MyString"
+phone_number "MyString"
+city "MyString"
+country "MyString"
+  end
+
+end
