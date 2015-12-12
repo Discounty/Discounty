409adf471f95d4f8a798546343bbdb1bc4f29aae
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-12 22:46:40 +0200

    Edit migration to remove not_null constraint from customer

--- db/migrate/20151207202703_create_customers.rb
+++ db/migrate/20151207202703_create_customers.rb
@@ -1,7 +1,7 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.string :first_name, null: false
+      t.string :first_name
       t.string :last_name
       t.string :email, null: false
       t.string :phone_number
