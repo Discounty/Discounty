9bd34563245e3433ef30a3ee28940ec961567e28
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-12 22:28:56 +0200

    Add devise routes and get rid of useless devise stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,27 @@
 Rails.application.routes.draw do
+
+  authenticated :customer do
+    root 'customers/home#index', as: :customer_root
+  end
+
+  authenticated :shop do
+    root 'shops/home#index', as: :shop_root
+  end
+
   root 'home#index'
 
   mount API::Base => '/api'
 
-  devise_for :shops
-  devise_for :customers
+  devise_for :shops, controllers: {
+    confirmations: 'shops/confirmations',
+    passwords: 'shops/passwords',
+    sessions: 'shops/sessions',
+    unlocks: 'shops/unlocks'
+  }
+  devise_for :customers, controllers: {
+    confirmations: 'customers/confirmations',
+    passwords: 'customers/passwords',
+    sessions: 'customers/sessions',
+    unlocks: 'customers/unlocks'
+  }
 end
