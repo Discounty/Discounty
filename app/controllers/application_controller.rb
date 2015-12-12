099d49a94588571d7cf1854a0427ed3ebffaebce
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-12 22:32:42 +0200

    Configure devise methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_filter :store_location
+
+  def store_location
+    session[:previous_url] =
+      request.fullpath if !request.fullpath.match('/customers') &&
+                          !request.fullpath.match('/shops') && !request.xhr?
+  end
+
+  def after_sign_in_path_for(_resource)
+    # session[:previous_url] || root_path
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:sign_in) << :email
+  end
 end
