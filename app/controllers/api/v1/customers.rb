9cbba7c0a8a211960a91d75701eeef29b86f7445
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-13 02:04:06 +0200

    Attempt use doorkeeper protection

--- app/controllers/api/v1/customers.rb
+++ app/controllers/api/v1/customers.rb
@@ -1,12 +1,23 @@
+require 'doorkeeper/grape/helpers'
+
 module API
   module V1
     class Customers < Grape::API
+      helpers Doorkeeper::Grape::Helpers
       include API::V1::Defaults
+
+      before do
+        doorkeeper_authorize!
+      end
 
       resource :customers do
         desc 'Return list of customers'
         get do
           Customer.all
+        end
+
+        get 'first' do
+          Customer.fisrt
         end
       end
     end
