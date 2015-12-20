1ee1c4f688410ffde54086adc6b795ddbbc8ddd8
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-20 18:40:11 +0200

    Add first version of Dashboard

--- app/controllers/customers/home_controller.rb
+++ app/controllers/customers/home_controller.rb
@@ -2,10 +2,30 @@ class Customers::HomeController < ApplicationController
   before_filter :authenticate_customer!
 
   def index
-    @personal_info_props = {
-      first_name: current_customer.first_name,
-      last_name: current_customer.last_name,
-      email: current_customer.email
+    # @personal_info_props = {
+    #   first_name: current_customer.first_name,
+    #   last_name: current_customer.last_name,
+    #   email: current_customer.email
+    # }
+    @customer_dashboard_props = {
+      cards: [
+        {
+          cardName: 'Card 1',
+          cardBarcode: '123456789'
+        },
+        {
+          cardName: 'Card 2',
+          cardBarcode: '987654321'
+        }
+      ],
+      navigationLinks: [
+        {
+          linkName: 'about'
+        },
+        {
+          linkName: 'coupons'
+        }
+      ]
     }
   end
 end
