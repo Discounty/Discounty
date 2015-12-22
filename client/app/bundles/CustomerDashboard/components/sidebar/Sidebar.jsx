57c56dc2e6ce1b6981a41c752e0e3698f46fda76
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-22 22:42:29 +0200

    Add sidebar components

---
+++ client/app/bundles/CustomerDashboard/components/sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import pureRender from 'pure-render-decorator';
+import SidebarItemsList from './SidebarItemsList';
+
+@pureRender
+export default class Sidebar extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    static propTypes = {
+        sidebarNavItems: PropTypes.arrayOf(PropTypes.shape({
+            itemString: PropTypes.string.isRequired,
+            itemUrl: PropTypes.string,
+            counter: PropTypes.integer,
+        })).isRequired,
+        sidebarLabelString: PropTypes.string,
+    }
+
+    render() {
+        return (
+            <nav className="cd-side-nav">
+                <SidebarItemsList
+                    sidebarNavItems={this.props.sidebarNavItems}
+                    sidebarLabelString={this.props.sidebarLabelString} />
+            </nav>
+        );
+    }
+}
