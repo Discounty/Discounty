11b554bae18dd485f5ff8d5e0d47d6501521c3f3
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-12-23 21:12:10 +0200

    Correct smtp spelling mistake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.default_url_options = { host: 'getdiscounty.com' }
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.stmp_settings = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: ENV['GMAIL_DOMAIN'],
